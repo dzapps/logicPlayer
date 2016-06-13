0a9611d2d8886f9d11ccc6b3200259dca0b3402e
Charlie Jaime <baroonie10@gmail.com>
2016-06-13 05:26:17 -0700

    add function to pbar pt1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
   // vars
   video = document.getElementById('video');
   playButton = document.getElementById('play-button');
+  pbar = document.getElementById('pbar');
 
 
   video.load();
@@ -15,8 +16,19 @@ function playOnPause(){
   if(video.paused){
     video.play();
     playButton.src = 'imgs/pause.png';
+    update = setInterval(updatePlayer, 30);
   } else{
     video.pause();
     playButton.src = 'imgs/play.png'
+    window.clearInterval(update);
+  }
+}
+
+function updatePlayer(){
+  var percentage = (video.currentTime/video.duration)*100;
+  pbar.style.width = percentage + '%';
+  if(video.ended){
+    window.clearInterval(update);
+    playButton.src = 'imgs/replay.png';
   }
 }
